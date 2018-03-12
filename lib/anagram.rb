17dee7e9294d65d3e54b455f26dac03f247045ac
Taylor Donlan <taylordonlan@gmail.com>
2018-03-12 17:51:19 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(potential_anagram_array)
+    confirmed_anagrams = []
+    potential_anagram_array.each do |potential_anagram|
+      if potential_anagram.split("").sort == @word.split("").sort
+        confirmed_anagrams << potential_anagram
+      end
+    end
+    confirmed_anagrams
+  end
+end
